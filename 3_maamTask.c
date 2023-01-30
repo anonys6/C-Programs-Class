7ec0d4399893c19c6be2dc1a6fa3c05bbd563fac
anonys6 <shakibalam998@gmail.com>
2023-01-30 16:18:50 +0530

    initial commit

---
+++ 3_maamTask.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main ()
+{
+    // printf("CSE101\n");
+    // printf("C - Programming\n");
+    // printf("LPU, India");
+
+    int age;
+
+    printf("Enter your age: \n");
+    scanf("%d", &age);
+
+    printf("Your age is %d", age);
+}
