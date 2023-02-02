bef5daef461f5b3f255bfa11187852f102313695
uplakshya <uplakshyakasyap@gmail.com>
2023-02-02 12:39:23 +0530

    jh

--- Class Questions/8_lec5_q1.c
+++ Class Questions/8_lec5_q1.c
@@ -2,17 +2,17 @@
 
 void main()
 {
-    // int age;
+    int age;
 
-    // printf("Enter your age: ");
-    // scanf("%d", &age);
-    // (age >= 18) ? (printf("Eligible\n")) : (printf("not Eligible\n"));
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    (age >= 18) ? (printf("Eligible\n")) : (printf("not Eligible\n"));
 
-    // int x, y, max;
-    // printf("Enter two numbers");
-    // scanf("%d %d", &x, &y);
-    // max = x > y? x : y;
-    // printf("Greater num is %d", x > y? x : y);
+    int x, y, max;
+    printf("Enter two numbers");
+    scanf("%d %d", &x, &y);
+    max = x > y? x : y;
+    printf("Greater num is %d", x > y? x : y);
     clrscr();
 
-}.;
+}
