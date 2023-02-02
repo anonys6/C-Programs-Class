119525ab84a7230872f4a28302749a1f3cef8aaf
uplakshya <uplakshyakasyap@gmail.com>
2023-02-02 12:38:49 +0530

    adding some new files

---
+++ Class Questions/6_Q1_operator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+    int a = 10, b = 9, c;
+
+    c = a && b;
+    printf("%d", c);
+}
---
+++ Class Questions/tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+
+    // int x, y, max;
+    // printf("Enter two numbers");
+    // scanf("%d %d", &x, &y);
+    // max = x > y? x : y;
+    // printf("Greater num is %d", x > y? x : y);
