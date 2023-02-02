a9b455895078cc82cccedbe578238589d3bb69a7
uplakshya <uplakshyakasyap@gmail.com>
2023-02-02 11:00:24 +0530

    doing nptel code

--- 9_bitwiseOperator.c
+++ 9_bitwiseOperator.c
@@ -1,12 +1,31 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     // printf("23 & 56: %d\n", 23 & 56);
+//     // printf("23 | 56: %d\n", 23 | 56);
+//     // printf("23 ^ 56: %d\n", 23 ^ 56);
+//     // printf("56 >> 2: %d\n", 56 >> 2);
+//     // printf("")
+
+
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
-int main()
+void main ()
 {
-    printf("23 & 56: %d\n", 23 & 56);
-    printf("23 | 56: %d\n", 23 | 56);
-    printf("23 ^ 56: %d\n", 23 ^ 56);
-    printf("56 >> 2: %d\n", 56 >> 2);
-    printf("")
-
-    return 0;
+  int m, n;
+  
+  scanf("%d%d", &m, &n);
+  if (m % n == 0)
+  {
+    printf("1");
+  }
+  else
+  {
+    printf("0");
+  }
 }
