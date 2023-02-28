94efb504ef132077e3e2b85fc27cdaa05d2ad0fd
anonys6 <shakibalam998@gmail.com>
2023-02-28 11:08:49 +0530

    goto and patterns

---
+++ 32_pattern.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // *
+    // * *
+    // * * *
+    // * * * *
+    // * * * * *
+
+    // int n = 5;
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         printf("* ");
+    //     }
+    //     printf("\n");
+    // }
+
+    // * * * * *
+    // * * * *
+    // * * *
+    // * *
+    // *
+
+    // int n = 5;
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     for (int j = 5; i < j; j--)
+    //     {
+    //         printf("* ");
+    //     }
+    //     printf("\n");
+    // }
+
+    //      *
+    //     * *
+    //    * * *
+    //   * * * *
+    //  * * * * *
+
+    // int n = 5;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int l = 5; i < l; l--)
+    //     {
+    //         printf(" ");
+    //     }
+    //     for (int m = 0; m <= i; m++)
+    //     {
+    //         printf("* ");
+    //     }
+    //     printf("\n");
+    // }
+
+    // int n = 9;
+
+    // for (int i = 0; i < n; i++)
+    // {
+
+    //     for (int l = 5; i < l; l--)
+    //     {
+    //         if (i >= 4)
+    //             break;
+    //         printf(" ");
+    //     }
+    //     for (int m = 0; m <= i; m++)
+    //     {
+    //         if (i >= 4)
+    //             break;
+    //         printf("* ");
+    //     }
+    //     printf("\n");
+
+    //     for (int j = 5; j <= i; j++)
+    //     {
+    //         if (i < 4)
+    //             break;
+    //         printf(" ");
+    //     }
+    //     for (int k = 9; i < k; k--)
+    //     {
+    //         if (i < 4)
+    //             break;
+    //         printf("* ");
+    //     }
+    //     printf("\n");
+    // }
+
+    return 0;
+}
---
+++ check.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // int a = 0;
+    // a = 5 || 2 | 1;
+    // printf("%d", a);
+
+    float f = 3.2;
+    printf("%f\n", f);
+    printf("%.1f\n", f);
+    printf("%.2f\n", f);
+    printf("%.4f\n", f);
+}
