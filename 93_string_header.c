b15dc2f7cb1b102fef5b49c076f72de797b59b24
anonys6 <shakibalam998@gmail.com>
2023-05-01 15:25:50 +0530

    string n compare and copy

--- 93_string_header.c
+++ 93_string_header.c
@@ -5,15 +5,17 @@ int main()
 {
     char str[] = "Hello";
     char str2[] = " World";
-    char *str3;
+    // char *str3;
     // strcpy(str, str3);
 
     printf("strrev(str): %s\n", strrev(str));
     printf("strlen(str): %d\n", strlen(str));
-    printf("strcmp(str, str2): %d\n", strcmp(strrev(str), str2));
+    printf("strcmp(str, str2): %d\n", strcmp(strrev(str), str2));       // +ve if s1 is greater
     printf("strcat(str, str2): %s\n", strcat(str, str2));
     printf("strupr(str): %s\n", strupr(str));
     printf("strlwr(str): %s\n", strlwr(str));
+
+    // printf("%s", strncpy("hello", "holiday", 4));
 
     return 0;
 }
