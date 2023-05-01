40912d25588b9db304beb997f437a5e790d3e272
anonys6 <shakibalam998@gmail.com>
2023-05-01 15:46:55 +0530

    stricmp and strncmp function

---
+++ 94_stringCompare.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+//  stricmp(s1, s2)     to compare two strings, case-insensitive
+
+int main() {
+    // char str1[] = "HELLO";
+    // char str2[] = "world";
+
+    // int x = strcmp(str1, str2);
+
+    // if (x == 0) {
+    //     printf("str1 is same as str2: %d", x);
+    // } else if (x > 0) {
+    //     printf("x is positive: %d", x);
+    // } else {
+    //     printf("x is negative: %d", x);
+    // }
+
+    char str3[] = "hell";
+    char str4[] = "hell0";
+
+    printf("strncmp(): %d\n", strncmp(str3, str4, 4));
+
+    char str5[] = "hell";
+    char str6[] = "HELL";
+    printf("stricmp(): %d\n", stricmp(str5, str6));
+
+    return 0;
+}
