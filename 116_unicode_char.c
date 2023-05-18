ef469fab53769966a62c1037d9c0fc94a505bd28
anonys6 <shakibalam998@gmail.com>
2023-05-18 12:53:35 +0530

    adding new files

---
+++ 116_unicode_char.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main () {
+    for (int i = 0; i < 200; i++) {
+        printf("%d = %c\t", i, i);
+    }
+}
--- 6_factorial.c
+++ 6_factorial.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
+int facto(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return (n * facto(n - 1));
+    }
+}
+
 void main()
 {
-    int facto(int n)
-    {
-        if (n == 1 || n == 0)
-        {
-            return 1;
-        }
-        else
-        {
-            return (n * facto(n - 1));
-        }
-    }
 
     int num = 0;
     int result = 0;
 
-    printf("Enter the number: \n");
+    printf("Enter the number: ");
     scanf("%d", &num);
 
     result = facto(num);
