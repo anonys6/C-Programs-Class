eea13e50fca279faaaf923e2bde4f1f384bcf1a6
anonys6 <shakibalam998@gmail.com>
2023-05-19 16:26:42 +0530

    garbage condition

--- check.c
+++ check.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-void main()
+
+int main()
 {
-    int k = 4;
-    int *const p = &k;
-    int r = 3;
-    p = &r;
-    printf("%d", p);
+    int x;
+    if (x)
+    {
+        printf("hi");
+    }
+    else
+    {
+        printf("hello");
+    }
+
+    return 0;
 }
