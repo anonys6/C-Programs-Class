43c95ef7878f72037c735da77d401e67c34ba3f0
anonys6 <shakibalam998@gmail.com>
2024-02-07 14:17:48 +0530

    script to change ownership of commits

---
+++ push.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="uplakshyakasyap@gmail.com"
+CORRECT_NAME="anonys6"
+CORRECT_EMAIL="shakibalam998@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
