4c96423da5daacdbfa9f6568ca40eb4b3aa6040e
anonys6 <shakibalam998@gmail.com>
2024-02-07 13:53:41 +0530

    formatting file

--- 1_hello.c
+++ 1_hello.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-void main()
-{
+void main() {
     printf("This is my first c program in class\n");
     printf("This is an another line in my first c program\n");
     printf("%d\n", 23 & 56);
